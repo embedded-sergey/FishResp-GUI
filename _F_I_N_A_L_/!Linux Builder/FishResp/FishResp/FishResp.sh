7dd9db3b52cc46671543ee89773d9612b185506a
RE.Kovalev <re.kovalev@gmail.com>
2023-09-03 11:04:00 +0300

    Fix linux package

--- _F_I_N_A_L_/!Linux Builder/FishResp/FishResp/FishResp.sh
+++ _F_I_N_A_L_/!Linux Builder/FishResp/FishResp/FishResp.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 sleep 5
-bash -c "export R_HOME=\"/usr/lib/R\" && cd '/usr/lib/FishResp' && java -jar FishRespBase.jar" </dev/null &>/dev/null &
+bash -c "export R_HOME=\"/usr/lib/R\" && cd '/usr/lib/FishResp' && java -Djdk.gtk.version=2 -Xss2560k -jar FishRespBase.jar" </dev/null &>/dev/null &
